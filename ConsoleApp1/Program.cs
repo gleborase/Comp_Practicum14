bc248c5d5e9d51dc3e05ba0f4a7044b3866177f5
gleborase <krasnopolsci@gmail.com>
2024-12-18 10:13:29 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Задание 2. Вывести на экран целые числа из интервала от 1 до n (цикл с параметром)
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Input("n=");
+            Func(n);
+            Console.Read();
+        }
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Func(int n)
+        {
+            for (int i=1; i <= n; i++) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+    }
+}
