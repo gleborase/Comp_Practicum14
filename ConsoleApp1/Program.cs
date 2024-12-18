854d63f014da4342fcba58931ce9df10e2031032
gleborase <krasnopolsci@gmail.com>
2024-12-18 09:44:30 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{/// <summary>
+/// Задание 1. Вывести на экран в столбик первые 10 натуральных чисел (циклы с условием, цикл с параметром)
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        static void Func() 
+        {
+            int x = 1;
+            while (x<=10) 
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+        }
+    }
+}
